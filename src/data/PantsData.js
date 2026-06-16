2908ef46c9c814b5ac0dd4757e77676a932cec53
israel boluwatife <israelboluwatife196@gmail.com>
2026-06-16 23:58:31 +0100

    adding Pantsdata

--- src/data/PantsData.js
+++ src/data/PantsData.js
@@ -1,63 +1,63 @@
-// import image1 from "../assets/images/MEN/Pants/Pant-1.webp";
-// import image2 from "../assets/images/MEN/Pants/Pant-10.webp";
-// import image3 from "../assets/images/MEN/Pants/Pant-11.webp";
-// import image4 from "../assets/images/MEN/Pants/Pant-12.webp";
-// import image5 from "../assets/images/MEN/Pants/Pant-13.webp";
-// import image6 from "../assets/images/MEN/Pants/Pant-14.webp";
-// import image7 from "../assets/images/MEN/Pants/Pant-15.webp";
-// import image8 from "../assets/images/MEN/Pants/Pant-16.webp";
-// import image9 from "../assets/images/MEN/Pants/Joggers.png";
-// import image10 from "../assets/images/MEN/Pants/Pant-4.webp";
+import image1 from "../assets/images/MEN/Pants/Pant-1.webp";
+import image2 from "../assets/images/MEN/Pants/Pant-10.webp";
+import image3 from "../assets/images/MEN/Pants/Pant-11.webp";
+import image4 from "../assets/images/MEN/Pants/Pant-12.webp";
+import image5 from "../assets/images/MEN/Pants/Pant-13.webp";
+import image6 from "../assets/images/MEN/Pants/Pant-14.webp";
+import image7 from "../assets/images/MEN/Pants/Pant-15.webp";
+import image8 from "../assets/images/MEN/Pants/Pant-16.webp";
+import image9 from "../assets/images/MEN/Pants/Joggers.png";
+import image10 from "../assets/images/MEN/Pants/Pant-4.webp";
 
-// export const PantsDatas = [
-//   {
-//     id: "Historic-ozo-agbada",
-//     name: "The Historic Ozo x NFL Agbada",
-//     price: 1273800,
-//     oldPrice: 1543000,
-//     inStock: 4,
-//     sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
-//     image: image1,
-//     hoverImage: image2,
-//   },
-//   {
-//     id: "Macallan-agbada",
-//     name: "Macallan Reserve Agbada",
-//     price: 885000,
-//     oldPrice: 1000000,
-//     inStock: 7,
-//     sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
-//     image: image3,
-//     hoverImage: image4,
-//   },
-//   {
-//     id: "Onìńówọ-Olori-aso-oke-agbada",
-//     name: "Onìńówọ & Olori Patched Aso-Oke Agbada",
-//     price: 3315000,
-//     oldPrice: 3450000,
-//     inStock: 3,
-//     sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
-//     image: image5,
-//     hoverImage: image6,
-//   },
-//   {
-//     id: "Turquoise-Blue-agbada",
-//     name: "Turquoise Blue Agbada Kaftan and Aso Oke Pant",
-//     price: 344300,
-//     oldPrice: 400000,
-//     inStock: 10,
-//     sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
-//     image: image7,
-//     hoverImage: image8,
-//   },
-//   {
-//     id: "Lion-king-agbada",
-//     name: "Lion King Agbada, Aso Oke Pant and Cap Set",
-//     price: 344300,
-//     oldPrice: 500000,
-//     inStock: 7,
-//     sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
-//     image: image9,
-//     hoverImage: image10,
-//   },
-// ];
+export const PantsDatas = [
+  {
+    id: "Historic-ozo-agbada",
+    name: "The Historic Ozo x NFL Agbada",
+    price: 1273800,
+    oldPrice: 1543000,
+    inStock: 4,
+    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    image: image1,
+    hoverImage: image2,
+  },
+  {
+    id: "Macallan-agbada",
+    name: "Macallan Reserve Agbada",
+    price: 885000,
+    oldPrice: 1000000,
+    inStock: 7,
+    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    image: image3,
+    hoverImage: image4,
+  },
+  {
+    id: "Onìńówọ-Olori-aso-oke-agbada",
+    name: "Onìńówọ & Olori Patched Aso-Oke Agbada",
+    price: 3315000,
+    oldPrice: 3450000,
+    inStock: 3,
+    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    image: image5,
+    hoverImage: image6,
+  },
+  {
+    id: "Turquoise-Blue-agbada",
+    name: "Turquoise Blue Agbada Kaftan and Aso Oke Pant",
+    price: 344300,
+    oldPrice: 400000,
+    inStock: 10,
+    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    image: image7,
+    hoverImage: image8,
+  },
+  {
+    id: "Lion-king-agbada",
+    name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 344300,
+    oldPrice: 500000,
+    inStock: 7,
+    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    image: image9,
+    hoverImage: image10,
+  },
+];
