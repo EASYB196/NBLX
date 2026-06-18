29bfd403f535d53cccd1e9defcab7c846325caec
israel boluwatife <israelboluwatife196@gmail.com>
2026-06-18 11:51:58 +0100

    updated jalabiya data images to lowercase

--- src/data/BestSellerData.js
+++ src/data/BestSellerData.js
@@ -2,8 +2,8 @@ import pinkcap from '../assets/images/CAPS/pinkcap.png';
 import pinkcap2 from '../assets/images/CAPS/pinkcap2.png';
 import blackcap from '../assets/images/CAPS/blackcap.png';
 import blackcap2 from '../assets/images/CAPS/blackcap2.png';
-import Armlesshoodie from '../assets/images/MEN/Hoodie/Armlesshoodie.png';
-import Armlesshoodiebackview from '../assets/images/MEN/Hoodie/Armlesshoodiebackview.png';
+import Armlesshoodie from '../assets/images/MEN/Hoodie/armlesshoodie.png';
+import Armlesshoodiebackview from '../assets/images/MEN/Hoodie/armlesshoodiebackview.png';
 
 import rhinestone from '../assets/images/CAPS/rhinestone.png';
 import rhinestone2 from '../assets/images/CAPS/rhinestone.png';
--- src/data/JalabiyaData.js
+++ src/data/JalabiyaData.js
@@ -1,99 +1,98 @@
-import image1 from "../assets/images/MEN/Jalabiya/Jalabiya-3.webp";
-import image2 from "../assets/images/MEN/Jalabiya/Jalabiya-4.webp";
-import image3 from "/src/assets/images/MEN/Jalabiya/DSC05551-Edit.webp";
-import image4 from "../assets/images/MEN/Jalabiya/Jalabiya-2.webp";
-import image5 from "../assets/images/MEN/Jalabiya/Jalabiya-5.webp";
-import image6 from "../assets/images/MEN/Jalabiya/Jalabiya-6.webp";
-import image7 from "../assets/images/MEN/Jalabiya/Jalabiya-7.webp";
-import image8 from "../assets/images/MEN/Jalabiya/Jalabiya-8.webp";
-import image9 from "../assets/images/MEN/Jalabiya/Jalabiya-9.webp";
-import image10 from "../assets/images/MEN/Jalabiya/Jalabiya-9.webp";
-import image11 from "../assets/images/MEN/Jalabiya/Jalabiya-10.webp";
-import image12 from "../assets/images/MEN/Jalabiya/Jalabiya-11.webp";
-import image13 from "../assets/images/MEN/Jalabiya/Jalabiya-12.webp";
-import image14 from "../assets/images/MEN/Jalabiya/Jalabiya-12.webp";
-import image15 from "../assets/images/MEN/Jalabiya/Jalabiya-13.webp";
-import image16 from "../assets/images/MEN/Jalabiya/Jalabiya-14.webp";
-
+import image1 from '../assets/images/MEN/Jalabiya/jalabiya-3.webp';
+import image2 from '../assets/images/MEN/Jalabiya/jalabiya-4.webp';
+import image3 from '/src/assets/images/MEN/Jalabiya/dsc05551-Edit.webp';
+import image4 from '../assets/images/MEN/Jalabiya/jalabiya-2.webp';
+import image5 from '../assets/images/MEN/Jalabiya/jalabiya-5.webp';
+import image6 from '../assets/images/MEN/Jalabiya/jalabiya-6.webp';
+import image7 from '../assets/images/MEN/Jalabiya/jalabiya-7.webp';
+import image8 from '../assets/images/MEN/Jalabiya/jalabiya-8.webp';
+import image9 from '../assets/images/MEN/Jalabiya/jalabiya-9.webp';
+import image10 from '../assets/images/MEN/Jalabiya/jalabiya-9.webp';
+import image11 from '../assets/images/MEN/Jalabiya/jalabiya-10.webp';
+import image12 from '../assets/images/MEN/Jalabiya/jalabiya-11.webp';
+import image13 from '../assets/images/MEN/Jalabiya/jalabiya-12.webp';
+import image14 from '../assets/images/MEN/Jalabiya/jalabiya-12.webp';
+import image15 from '../assets/images/MEN/Jalabiya/jalabiya-13.webp';
+import image16 from '../assets/images/MEN/Jalabiya/jalabiya-14.webp';
 
 export const JalabiyaDatas = [
   {
-    id: "noble-jalabiya",
-    name: " Man Hand-painted Jalabiya",
+    id: 'noble-jalabiya',
+    name: ' Man Hand-painted Jalabiya',
     price: 255000,
     oldPrice: 300000,
     inStock: 5,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image1,
     hoverImage: image2,
   },
   {
-    id: "line-art-jalabiya",
-    name: "Line Art Jalabiya and Cap Set",
+    id: 'line-art-jalabiya',
+    name: 'Line Art Jalabiya and Cap Set',
     price: 255000,
     oldPrice: 300000,
     inStock: 7,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image3,
     hoverImage: image4,
   },
   {
-    id: "Hand-painted-jalabiya",
-    name: "Hand painted V-Neck  Jalabiya",
+    id: 'Hand-painted-jalabiya',
+    name: 'Hand painted V-Neck  Jalabiya',
     price: 318000,
     oldPrice: 420000,
     inStock: 6,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image5,
     hoverImage: image6,
   },
   {
-    id: "Line-art-jalabiya",
-    name: "Line Art Jalabiya",
+    id: 'Line-art-jalabiya',
+    name: 'Line Art Jalabiya',
     price: 255000,
     oldPrice: 300000,
     inStock: 10,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image7,
     hoverImage: image8,
   },
   {
-    id: "Map-of-africa-jalabiya",
-    name: "Map of Africa Hand Painted Jalabiya",
+    id: 'Map-of-africa-jalabiya',
+    name: 'Map of Africa Hand Painted Jalabiya',
     price: 255000,
     oldPrice: 350000,
     inStock: 13,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image9,
     hoverImage: image10,
   },
   {
-    id: "Male-hand-painted-jalabiya",
-    name: "Male Hand-Painted Jalabiya",
+    id: 'Male-hand-painted-jalabiya',
+    name: 'Male Hand-Painted Jalabiya',
     price: 255000,
     oldPrice: 300000,
     inStock: 6,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image11,
     hoverImage: image12,
   },
   {
-    id: "Benin-jalabiya",
-    name: "Benin Head Painted Jalabiya",
+    id: 'Benin-jalabiya',
+    name: 'Benin Head Painted Jalabiya',
     price: 255000,
     oldPrice: 300000,
     inStock: 15,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image13,
     hoverImage: image14,
   },
   {
-    id: "Line-art-white-jalabiya",
-    name: "Line Art White Jalabiya and Cap Set",
-    price: 255000, 
+    id: 'Line-art-white-jalabiya',
+    name: 'Line Art White Jalabiya and Cap Set',
+    price: 255000,
     oldPrice: 300000,
     inStock: 8,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XS'],
     image: image15,
     hoverImage: image16,
   },
